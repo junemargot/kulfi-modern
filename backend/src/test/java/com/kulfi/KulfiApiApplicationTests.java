6ab76724dbb94eb78c69198aea3518a9c1e05f20
EJH <margotinjune@gmail.com>
2026-04-06 17:34:50 +0900

    Initial commit: Monorepo setup with backend and frontend

---
+++ backend/src/test/java/com/kulfi/KulfiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kulfi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KulfiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
